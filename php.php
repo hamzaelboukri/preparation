8d6b4d197c2a377c9f556a4a2c45cc5b47586ee2
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-04-28 21:01:47 +0100

    hgda

---
+++ php.php
@@ -0,0 +1,86 @@
+<?php
+
+
+interface Tree {
+  
+    public function sound();
+    public function die();
+}
+
+
+abstract class  Personne implements Tree {
+  protected int $id;
+  protected string $name;
+  protected string $lastname;
+  protected string $gender;
+
+
+  public function __construct( $id,$name,$lastname) {
+    $this->id= $id;
+    $this->name=$name;
+    $this->lastname=$lastname;
+  }
+
+
+public abstract function gender ();
+
+  public function sound(){
+    echo " saye helle word";
+    
+  }
+
+  public function die(){
+    echo "im diening and . $this->name";
+    
+  }
+
+}
+
+
+
+class Male extends Personne {
+    public function __construct($id, $name, $lastname) {
+        parent::__construct($id, $name, $lastname);
+    }
+
+    public function gender() {
+        echo "I am " . $this->name;
+    }
+
+
+    public function sound(){
+        echo " saye helle word". $this->lastname;
+        
+      }
+    
+      public function die(){
+        echo "im diening and  $this->name";
+        
+      }
+}
+
+
+
+
+class Femmail extends Personne {
+
+public function __construct($id,$name,$lastname) {
+    parent::__construct($id,$name,$lastname) ;
+
+}
+
+
+public function gender() {
+    echo   $this->name;
+}
+
+public function sound(){
+    echo " saye helle word". $this->lastname;
+    
+  }
+
+  public function die(){
+    echo "im diening and  $this->name";
+    
+  }
+}
