b0978712fe066d4a214ee79f7ebac8608f0ed2f6
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-04-28 18:22:19 +0100

    sdsu

--- script.js
+++ script.js
@@ -91,14 +91,52 @@ console.log(calculatrice.darb(4 ,2))
 
 
 
+let tab1=[ 1,3,4,5,6,]
+let tab2=[2,3,4,7]
+let tab3=[];
+
+for (let i = 0; i < tab1.length; i++) {
+  for (let j = 0; j < tab2.length; j++) {
+
+    if (tab1[i]==tab2[j]) {
+        tab3.push(tab1[i])
+         }  
+}    
+}
+console.log(tab3)
 
 
 
 
+// ========================================================================
 
 
+let name ="aaaannnnsjssss" ;
+let counts =[];
 
+for (let i = 0; i < name.length; i++) {
+    let char = name[i];
+    if (counts[char]) {
+        counts[char]++;
+    } else {
+        counts[char] = 1;
+    }
+}
 
+console.log(counts);
+
+console.log("=======================================")
+
+let tab=[2,3,4,5,6,7,8,2];
+
+let sum=[]
+
+for (let i = 0; i < tab.length; i++) {
+    
+    sum=tab[i]+tab[i]+1;
+    console.log(sum)
+
+}
 
 
 
